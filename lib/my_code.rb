45297fccda5da85aef84586af13831815033952d
Victoria Hale <vic.hale74@gmail.com>
2019-10-01 16:53:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# Your Code Here
+def map (source_array)# Your Code Here
+  i = 0
+  new_array = []
+
+  while i < source_array.count do 
+    new_array.push(yield(source_array[i]))
+  
+    
+    i += 1
+  end
+ 
+  return new_array
+end
+
+def reduce (source_array, starting_point={})
+  i = 0
+  
+  while i < source_array.count do
+    total += source_array[i]
+    yield(source_array)
+    
+    i += 1 
+    
+  end
+  total
+ end
+
+
+
+
+
