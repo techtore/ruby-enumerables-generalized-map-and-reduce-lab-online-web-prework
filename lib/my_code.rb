d6d91d59d5767584ef85a5d36326df92084def4f
Victoria Hale <vic.hale74@gmail.com>
2019-10-01 17:50:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,7 @@ def map (source_array)# Your Code Here
   return new_array
 end
 
-def reduce (source_array, starting_point) do |t|
-  total = source_array + starting_point
-  yield(total)
-  end 
- end
+
 
 
 
